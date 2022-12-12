83efab271700f0d30e5d5519b0fca12e722e4ddf
Daniel-Maslyanikov <mddm0113@gmail.com>
2022-12-12 13:01:36 +0300

    added counter func

---
+++ main.js
@@ -0,0 +1,21 @@
+// DISPLAY COST
+window.addEventListener('click', function (event) {
+  if (event.target.dataset.action === 'plus') {
+    const counterItem = event.target.closest('.item__counter');
+    const counter = counterItem.querySelector('[data-display]');
+		const amount = event.target.closest('.item__interaction').querySelector('.amount')
+		
+    if (parseInt(counter.innerText) < parseInt(amount.innerText)) {
+      counter.innerText = ++counter.innerText;
+    } 
+  }
+
+  if (event.target.dataset.action === 'minus') {
+    const counterItem = event.target.closest('.item__counter');
+    const counter = counterItem.querySelector('[data-display]');
+
+    if (parseInt(counter.innerText) > 1) {
+      counter.innerText = --counter.innerText;
+    }
+  }
+});
