0c087dfd599fa62b97852b8c24e68a031cf5bb95
Daniel-Maslyanikov <mddm0113@gmail.com>
2023-01-31 14:50:37 +0300

    fixed elemHider func

--- js/elementsHider.js
+++ js/elementsHider.js
@@ -1,19 +1,31 @@
-const hider = document.getElementById('hider');
-const hidden = document.getElementById('section');
-hidden.style.display = 'block'
+const inStockHider = document.getElementById('hider-in-stock');
+const inStock = document.getElementById('in-stock');
+inStock.style.display = 'block';
+
+const outOfStockHider = document.getElementById('hider-out-of-stock');
+const outOfStock = document.getElementById('out-of-stock');
+outOfStock.style.display = 'block';
 
 
 
-hider.onclick = function () {
-	if (hidden.style.display == 'block') {
-		hidden.style.display = 'none';
-		hider.style.transform = 'rotate(180deg)'
-	} else if (hidden.style.display == 'none') {
-		hidden.style.display = 'block';
-		hider.style.transform = 'rotate(180deg)'
+outOfStockHider.onclick = function () {
+	if (outOfStock.style.display == 'block') {
+		outOfStock.style.display = 'none';
+		outOfStockHider.style.transform = 'rotate(180deg)'
+	} else if (outOfStock.style.display == 'none') {
+		outOfStockHider.style.transform = 'rotate(360deg)'
+		outOfStock.style.display = 'block';
+		
 	} 
+}
 
-	
-	
-
+inStockHider.onclick = function () {
+	if (inStock.style.display == 'block') {
+		inStock.style.display = 'none';
+		inStockHider.style.transform = 'rotate(180deg)'
+	} else if (inStock.style.display == 'none') {
+		inStockHider.style.transform = 'rotate(360deg)'
+		inStock.style.display = 'block';
+		
+	} 
 }
