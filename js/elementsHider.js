729a313cbb6499b383b6b8f5ef45b4e5a14d241d
Daniel-Maslyanikov <mddm0113@gmail.com>
2023-02-01 18:48:14 +0300

    fix elemHider func

--- js/elementsHider.js
+++ js/elementsHider.js
@@ -1,6 +1,9 @@
 const inStockHider = document.getElementById('hider-in-stock');
 const inStock = document.getElementById('in-stock');
 inStock.style.display = 'block';
+const inStockAll = document.getElementById('checkbox-all');
+const textAll = document.getElementById('text-all');
+
 
 const outOfStockHider = document.getElementById('hider-out-of-stock');
 const outOfStock = document.getElementById('out-of-stock');
@@ -21,9 +24,15 @@ outOfStockHider.onclick = function () {
 
 inStockHider.onclick = function () {
 	if (inStock.style.display == 'block') {
+		textAll.textContent = `? товаров · ${price.total} сом`
+		textAll.classList.add("text--primary")
+		inStockAll.style.display = 'none';
 		inStock.style.display = 'none';
 		inStockHider.style.transform = 'rotate(180deg)'
 	} else if (inStock.style.display == 'none') {
+		inStockAll.style.display = 'block';
+		textAll.textContent = `Выбрать все`
+		textAll.classList.remove("text--primary")
 		inStockHider.style.transform = 'rotate(360deg)'
 		inStock.style.display = 'block';
 		
